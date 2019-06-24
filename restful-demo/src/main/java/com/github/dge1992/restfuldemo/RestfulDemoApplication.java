e20dc258036e9b935ffe81701c688fe3dde2a92e
dge1992 <dge_1992@163.com>
2019-06-24 17:29:12 +0800

    springboot RESTFulAPI联系及集成Swagger

---
+++ restful-demo/src/main/java/com/github/dge1992/restfuldemo/RestfulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.dge1992.restfuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulDemoApplication.class, args);
+    }
+
+}

67a313110a196c395875759246eafbf3c80586ce
dge1992 <dge_1992@163.com>
2019-06-28 15:10:42 +0800

    多数据源

---
+++ multiple-data-sources/src/main/java/com/github/dge1992/multipledatasources/MultipleDataSourcesApplication.java
@@ -0,0 +1,13 @@
+package com.github.dge1992.multipledatasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDataSourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleDataSourcesApplication.class, args);
+    }
+
+}
---
+++ multiple-data-sources/src/main/java/com/github/dge1992/multipledatasources/config/DataSourceConfig.java
@@ -0,0 +1,45 @@
+package com.github.dge1992.multipledatasources.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author 小眼睛带鱼
+ * @Description
+ * @Date 2019/6/27
+ **/
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "primaryDataSource")
+    @ConfigurationProperties(prefix="spring.datasource.primary")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "secondaryDataSource")
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource.secondary")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "primaryJdbcTemplate")
+    public JdbcTemplate primaryJdbcTemplate(
+            @Qualifier("primaryDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = "secondaryJdbcTemplate")
+    public JdbcTemplate secondaryJdbcTemplate(
+            @Qualifier("secondaryDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}

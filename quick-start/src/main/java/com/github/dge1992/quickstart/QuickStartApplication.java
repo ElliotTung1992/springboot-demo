4b6db946f165cf68cac41d631f0edca4e193071f
dge1992 <dge_1992@163.com>
2019-07-08 17:45:23 +0800

    定时任务

--- quick-start/src/main/java/com/github/dge1992/quickstart/QuickStartApplication.java
+++ quick-start/src/main/java/com/github/dge1992/quickstart/QuickStartApplication.java
@@ -2,7 +2,9 @@ package com.github.dge1992.quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class QuickStartApplication {
 

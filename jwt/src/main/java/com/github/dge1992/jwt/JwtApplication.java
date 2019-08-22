444775180fd710df7813e470c09c755a28b4c43a
dge1992 <dge_1992@163.com>
2019-08-22 16:45:46 +0800

    JWT签名验签

---
+++ jwt/src/main/java/com/github/dge1992/jwt/JwtApplication.java
@@ -0,0 +1,13 @@
+package com.github.dge1992.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtApplication.class, args);
+    }
+
+}

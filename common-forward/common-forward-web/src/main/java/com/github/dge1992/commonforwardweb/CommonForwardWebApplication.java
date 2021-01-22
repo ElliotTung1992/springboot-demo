08cc05550d2bab7788f99b1f8997035b0ef23090
dongbinyang <dge_1992@163.com>
2021-01-22 16:00:26 +0800

    添加测试用例及注释

--- common-forward/common-forward-web/src/main/java/com/github/dge1992/commonforwardweb/CommonForwardWebApplication.java
+++ common-forward/common-forward-web/src/main/java/com/github/dge1992/commonforwardweb/CommonForwardWebApplication.java
@@ -27,6 +27,8 @@ import org.springframework.context.annotation.ComponentScans;
  * 4. 自动重试
  * 4.1 部分Http异常自动重试
  * 4.2 部分业务异常自动重试
+ * 5. 异步支持
+ * 5.1 除消息队列调用其他调用方式需要异步调用
  */
 @ComponentScan(basePackages = {"com.github.dge1992"})
 @SpringBootApplication

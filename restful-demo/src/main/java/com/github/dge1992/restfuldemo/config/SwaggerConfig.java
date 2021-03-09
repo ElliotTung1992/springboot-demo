88e759fc39991f608342698903ff228ef6bd6dcf
dongbinyang <dge_1992@163.com>
2021-03-09 14:42:55 +0800

    小改动

--- restful-demo/src/main/java/com/github/dge1992/restfuldemo/config/SwaggerConfig.java
+++ restful-demo/src/main/java/com/github/dge1992/restfuldemo/config/SwaggerConfig.java
@@ -28,7 +28,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.DocumentationCache;
 import springfox.documentation.spring.web.json.Json;
 import springfox.documentation.spring.web.json.JsonSerializer;
-import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.ApiResourceController;
 import springfox.documentation.swagger.web.SecurityConfiguration;
--- restful-demo/src/main/java/com/github/dge1992/restfuldemo/controller/UserController.java
+++ restful-demo/src/main/java/com/github/dge1992/restfuldemo/controller/UserController.java
@@ -4,12 +4,15 @@ import com.github.dge1992.restfuldemo.domain.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author 小眼睛带鱼
@@ -20,6 +23,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequestMapping("/user")
 @RestController
 public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
     Map<Long, User> map = new ConcurrentHashMap<>();
 
@@ -33,17 +38,10 @@ public class UserController {
     @ApiOperation(value = "获取用户列表", notes = "获取用户列表")
     @GetMapping
     public List<User> getUserList(){
-        User user = new User();
-        user.setId(1L);
-        user.setName("dge");
-        user.setAge(11);
-        User user2 = new User();
-        user2.setId(2L);
-        user2.setName("fnn");
-        user2.setAge(12);
-        map.put(1L, user);
-        map.put(2L, user2);
-        return new ArrayList<>(map.values());
+        logger.info("getUserList开始调用");
+        List<User> users = returnUserList();
+        logger.info("getUserList结束调用");
+        return users;
     }
 
     @ApiOperation(value = "获取用户详情", notes = "获取用户详情")
@@ -64,5 +62,30 @@ public class UserController {
     public String deleteUser(@ApiParam(value = "主键编号", required = true) @RequestParam("id") long id){
         map.remove(id);
         return "删除成功";
+    }
+
+    /**
+     * 构建UserList
+     * @return java.util.List<com.github.dge1992.restfuldemo.domain.User>
+     * @author dge
+     * @date 2021-03-08 17:16
+     */
+    private List<User> returnUserList(){
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        User user = new User();
+        user.setId(1L);
+        user.setName("dge");
+        user.setAge(11);
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setName("fnn");
+        user2.setAge(12);
+        map.put(1L, user);
+        map.put(2L, user2);
+        return new ArrayList<>(map.values());
     }
 }

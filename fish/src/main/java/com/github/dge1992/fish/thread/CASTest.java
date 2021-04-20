c5d0566e3c2439fe02f755be2e1ee9a9718a1702
dongbinyang <dge_1992@163.com>
2021-04-20 23:12:39 +0800

    CAS案例及问题

--- fish/src/main/java/com/github/dge1992/fish/thread/CASTest.java
+++ fish/src/main/java/com/github/dge1992/fish/thread/CASTest.java
@@ -1,5 +1,6 @@
 package com.github.dge1992.fish.thread;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -11,19 +12,64 @@ public class CASTest {
 
     AtomicInteger atomicInteger = new AtomicInteger();
 
+    private volatile Integer i = 0;
+
+    private static final Integer ROUND = 10000;
+
+    private CountDownLatch countDownLatch = new CountDownLatch(ROUND);
+
     public static void main(String[] args) {
         CASTest test = new CASTest();
-        test.testOne();
 
-        /*Integer i = 99;
-        Integer j = i;
-        j = j + 0;
+        //test.testOne();
+        //test.two();
+        test.three();
+    }
 
-        System.out.println(i == j);
-        System.out.println(i.equals(j));*/
+    /**
+     * 模拟多线程原子性问题
+     * @author dge
+     * @date 2021-04-20 22:46
+     */
+    private void three() {
+        for (int j = 0; j < ROUND; j++) {
+            new Thread(() -> {
+                for (int k = 1; k <= 100; k++) {
+                    add();
+                }
+                countDownLatch.countDown();
+            }).start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(atomicInteger.get());
+        System.out.println(i);
 
     }
 
+    public void add(){
+        atomicInteger.incrementAndGet();
+        i++;
+    }
+
+    /**
+     * 线程工具方法使用
+     * @author dge
+     * @date 2021-04-20 22:45
+     */
+    private void two() {
+        Thread.currentThread().getThreadGroup().list();
+        System.out.println(Thread.activeCount());
+    }
+
+    /**
+     * 创建api使用
+     * @author dge
+     * @date 2021-04-20 22:45
+     */
     private void testOne() {
         //System.out.println(atomicInteger.get());
 

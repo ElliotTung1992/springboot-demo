2875e5e5818c98073c291b76aa95e31a125006d8
dongbinyang <dge_1992@163.com>
2022-03-27 22:23:58 +0800

    feat: 广度优先搜索之岛屿的最大面积

--- algorithm/src/main/java/com/github/dge/algorithm/depthfirstsearch/MaxAreaOfIsland.java
+++ algorithm/src/main/java/com/github/dge/algorithm/depthfirstsearch/MaxAreaOfIsland.java
@@ -30,11 +30,38 @@ public class MaxAreaOfIsland {
         System.out.println(i);
     }
 
+    private int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private int m, n;
+
     /**
-     * 核心思想：
-     * 求遇队列的最大值
+     * DFS的思想
      */
     public int maxAreaOfIsland(int[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        int maxCount = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                maxCount = Math.max(maxCount, dfs(grid, i, j));
+            }
+        }
+        return maxCount;
+    }
+
+    private int dfs(int[][] grid, int cm, int cn){
+        int count = 0;
+        if(cm < 0 || cm >= m || cn < 0 || cn >= n || grid[cm][cn] == 0){
+            return count;
+        }
+        count++;
+        grid[cm][cn] = 0;
+        for (int[] direction : directions) {
+            count += dfs(grid, cm + direction[0], cn + direction[1]);
+        }
+        return count;
+    }
+
+    public int maxAreaOfIsland2(int[][] grid) {
         int[][] direction = {{-1, 0}, {1, 0}, {0, 1}};
         int m = grid.length;
         int n = grid[0].length;

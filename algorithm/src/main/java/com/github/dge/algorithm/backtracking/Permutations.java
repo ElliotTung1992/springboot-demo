6824afaa69a8d11cca767db4077334c6d7eb45e2
dongbinyang <dge_1992@163.com>
2022-04-05 21:34:14 +0800

    feat: 回溯之全排列

--- algorithm/src/main/java/com/github/dge/algorithm/backtracking/Permutations.java
+++ algorithm/src/main/java/com/github/dge/algorithm/backtracking/Permutations.java
@@ -1,6 +1,7 @@
 package com.github.dge.algorithm.backtracking;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,12 +16,40 @@ public class Permutations {
 
     public static void main(String[] args) {
         Permutations permutations = new Permutations();
-        int[] nums = {1};
+        int[] nums = {1,2,3};
         List<List<Integer>> list = permutations.permute(nums);
         System.out.println(list);
     }
 
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+
+    public List<List<Integer>> permute2(int[] nums) {
         int length = nums.length;
         boolean[] bArr = new boolean[length];
         List<List<Integer>> list = new ArrayList<>();

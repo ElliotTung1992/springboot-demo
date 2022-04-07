6f5d1f74017f3b0f70b46b45215b1be1b10e7d4a
dongbinyang <dge_1992@163.com>
2022-04-07 10:55:56 +0800

    feat: 回溯之组合总和

--- algorithm/src/main/java/com/github/dge/algorithm/backtracking/CombinationSum.java
+++ algorithm/src/main/java/com/github/dge/algorithm/backtracking/CombinationSum.java
@@ -22,6 +22,31 @@ public class CombinationSum {
         System.out.println(list);
     }
 
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+
+    public void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+        if (idx == candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            ans.add(new ArrayList<Integer>(combine));
+            return;
+        }
+        // 直接跳过
+        dfs(candidates, target, ans, combine, idx + 1);
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            combine.add(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.remove(combine.size() - 1);
+        }
+    }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> dataList = new ArrayList<>();

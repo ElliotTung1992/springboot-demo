052f1c09e6ba5027c9242eebf2ebdde8a212cddc
dongbinyang <dge_1992@163.com>
2022-08-09 16:54:05 +0800

    feat: math - produce of array except self

--- algorithm/src/main/java/com/github/dge/algorithm/math/ProductOfArrayExceptSelf.java
+++ algorithm/src/main/java/com/github/dge/algorithm/math/ProductOfArrayExceptSelf.java
@@ -14,12 +14,49 @@ public class ProductOfArrayExceptSelf {
 
     public static void main(String[] args) {
         ProductOfArrayExceptSelf productOfArrayExceptSelf = new ProductOfArrayExceptSelf();
+        // int[] nums = {1,2,3,4};
         int[] nums = {-1,1,0,-3,3};
         int[] ints = productOfArrayExceptSelf.productExceptSelf(nums);
         System.out.println(Arrays.toString(ints));
     }
 
     public int[] productExceptSelf(int[] nums) {
+        int length = nums.length, R = 1;
+        int[] result = new int[length];
+        result[0] = 1;
+        for (int i = 1; i < length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        for (int i = length - 1; i >= 0 ; i--) {
+            result[i] = result[i] * R;
+            R *= nums[i];
+        }
+        return result;
+    }
+
+    public int[] productExceptSelf3(int[] nums){
+        int length = nums.length;
+        int[] LEFT = new int[length];
+        int[] RIGHT = new int[length];
+        int[] result = new int[nums.length];
+        LEFT[0] = nums[0];
+        for (int i = 1; i < length; i++) {
+            LEFT[i] = LEFT[i -1] * nums[i];
+        }
+        RIGHT[length - 1] = nums[length - 1];
+        for (int i = length - 2; i > -1; i--) {
+            RIGHT[i] = RIGHT[i + 1] * nums[i];
+        }
+
+        result[0] = RIGHT[1];
+        result[length - 1] = LEFT[length - 2];
+        for (int i = 1; i < length -1; i++) {
+            result[i] = LEFT[i - 1] * RIGHT[i + 1];
+        }
+        return result;
+    }
+
+    public int[] productExceptSelf2(int[] nums) {
         int allProduct = 1, zeroCount = 0;
         for (int num : nums) {
             if(num == 0){

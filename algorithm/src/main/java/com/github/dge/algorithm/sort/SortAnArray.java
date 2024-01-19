db03d50d0356d3429bf7d45d6f8a4799e0b810d4
Elliot <dge_1992@163.com>
2024-01-19 16:33:53 +0800

    feat: 删除没用的代码

--- algorithm/src/main/java/com/github/dge/algorithm/sort/SortAnArray.java
+++ algorithm/src/main/java/com/github/dge/algorithm/sort/SortAnArray.java
@@ -1,7 +1,6 @@
 package com.github.dge.algorithm.sort;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class SortAnArray {
 
@@ -13,6 +12,56 @@ public class SortAnArray {
     }
 
     public int[] sortArray(int[] nums) {
+        heapSort(nums);
+        return nums;
+    }
+
+    public void heapSort(int[] nums) {
+        int len = nums.length - 1;
+        // 构建大根堆
+        buildMaxHeap(nums, len);
+        for (int i = len; i >= 1; --i) {
+            swap(nums, i, 0);
+            len -= 1;
+            maxHeapify(nums, 0, len);
+        }
+    }
+
+    public void buildMaxHeap(int[] nums, int len) {
+        for (int i = len / 2; i >= 0; --i) {
+            maxHeapify(nums, i, len);
+        }
+    }
+
+    public void maxHeapify(int[] nums, int i, int len) {
+        for (; (i << 1) + 1 <= len;) {
+            int lson = (i << 1) + 1;
+            int rson = (i << 1) + 2;
+            int large;
+            if (lson <= len && nums[lson] > nums[i]) {
+                large = lson;
+            } else {
+                large = i;
+            }
+            if (rson <= len && nums[rson] > nums[large]) {
+                large = rson;
+            }
+            if (large != i) {
+                swap(nums, i, large);
+                i = large;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    /*public int[] sortArray(int[] nums) {
         quickSort(nums, 0, nums.length - 1);
         return nums;
     }
@@ -58,7 +107,7 @@ public class SortAnArray {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-    }
+    }*/
 
 
 
--- jwt/src/main/java/com/github/dge1992/jwt/validator/IAuthValidator.java
+++
@@ -1,8 +0,0 @@
-package com.github.dge1992.jwt.validator;
-
-import com.github.dge1992.jwt.doamin.AuthObject;
-
-public interface IAuthValidator {
-
-    boolean validate(AuthObject authObject);
-}

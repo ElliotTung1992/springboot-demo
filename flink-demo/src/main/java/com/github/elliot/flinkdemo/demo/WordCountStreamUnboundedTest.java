14f3f5996a328901f3ae08ba1cc0ad9e3ecb5bd5
Elliot <dge_1992@163.com>
2024-06-03 21:09:51 +0800

    feat: Flink

--- flink-demo/src/main/java/com/github/elliot/flinkdemo/demo/WordCountStreamUnboundedTest.java
+++ flink-demo/src/main/java/com/github/elliot/flinkdemo/demo/WordCountStreamUnboundedTest.java
@@ -4,6 +4,7 @@ import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -14,7 +15,12 @@ public class WordCountStreamUnboundedTest {
 
     public static void main(String[] args) throws Exception {
         // 创建环境
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+
+        env.setParallelism(1);
+        // 全局禁用算子链合并
+        // env.disableOperatorChaining();
         // 读取数据
         // DataStreamSource<String> stringDataStreamSource = env.readTextFile("flink-demo/data/word_count.txt");
         // 读取数据
@@ -29,9 +35,11 @@ public class WordCountStreamUnboundedTest {
                         out.collect(tuple2);
                     }
                 })
+                //.setParallelism(2)
                 .returns(Types.TUPLE(Types.STRING, Types.INT))
                 .keyBy((KeySelector<Tuple2<String, Integer>, Object>) value -> value.f0)
                 .sum(1)
+                //.disableChaining()
                 .print();
 
         // 执行

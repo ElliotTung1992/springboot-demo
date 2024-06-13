42212f9a7adc99a5d2da9f5c86c5096608bbb107
Elliot <dge_1992@163.com>
2024-06-13 20:46:17 +0800

    feat: 事务消息

--- rocketmq/src/main/java/com/github/elliot/rocketmq/transaction/domain/Order.java
+++ rocketmq/src/main/java/com/github/elliot/rocketmq/transaction/domain/Order.java
@@ -1,7 +1,9 @@
 package com.github.elliot.rocketmq.transaction.domain;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+@TableName("order")
 @Data
 public class Order {
 

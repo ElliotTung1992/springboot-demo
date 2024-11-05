054c64a0a35ffea5076d1de6f3055e5131122d86
Elliot <dge_1992@163.com>
2024-11-05 14:26:02 +0800

    feat: flink-window - 增量聚合

---
+++ flink-demo/src/main/java/com/github/elliot/flinkdemo/window/WindowReduceDemo.java
@@ -0,0 +1,50 @@
+package com.github.elliot.flinkdemo.window;
+
+import com.github.elliot.flinkdemo.bean.WaterSensor;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+public class WindowReduceDemo {
+
+    public static void main(String[] args) throws Exception {
+        LocalStreamEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
+        SingleOutputStreamOperator<WaterSensor> streamOperator = env.socketTextStream("10.211.55.4", 7777)
+                .flatMap(new FlatMapFunction<String, WaterSensor>() {
+                    @Override
+                    public void flatMap(String value, Collector<WaterSensor> out) throws Exception {
+                        String[] split = value.split(",");
+                        WaterSensor waterSensor = new WaterSensor();
+                        waterSensor.setId(split[0]);
+                        waterSensor.setSt(Long.valueOf(split[1]));
+                        waterSensor.setVc(Integer.valueOf(split[2]));
+                        out.collect(waterSensor);
+                    }
+                });
+        KeyedStream<WaterSensor, String> waterSensorStringKeyedStream =
+                streamOperator.keyBy(e -> e.getId());
+
+        WindowedStream<WaterSensor, String, TimeWindow> window
+                = waterSensorStringKeyedStream.window(TumblingProcessingTimeWindows.of(Time.seconds(10)));
+
+        SingleOutputStreamOperator<WaterSensor> reduce = window.reduce(new ReduceFunction<WaterSensor>() {
+            @Override
+            public WaterSensor reduce(WaterSensor value1, WaterSensor value2) throws Exception {
+                System.out.println("receive source value1:" + value1 + "value2:" + value2);
+                return new WaterSensor(value1.getId(), value1.getSt(), value1.getVc() + value2.getVc());
+            }
+        });
+
+        reduce.print();
+
+        env.execute();
+    }
+}
